78759d6b8765618f9c35c17af7b0c3ad57c91e65
Филимонов Владислав Валерьевич <v.filimonov@sibset-team.ru>
2021-10-07 20:49:55 +0700

    STP Lab 2

---
+++ 4th-year/stp/Lab2/Lab2.tests/Lab2Tests.cs
@@ -0,0 +1,100 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab2.tests
+{
+    [TestClass]
+    public class Lab2Tests
+    {
+        [TestMethod]
+        public void FirstFunctionTest1()
+        {
+            Lab lab = new Lab();
+
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            int expected = 0;
+
+            int actual = lab.FirstFunction(num1, num2, num3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FirstFunctionTest2()
+        {
+            Lab lab = new Lab();
+
+            int num1 = 14;
+            int num2 = 3;
+            int num3 = -7;
+            int expected = -7;
+
+            int actual = lab.FirstFunction(num1, num2, num3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SecondFunctionTest1()
+        {
+            Lab lab = new Lab();
+
+            double[,] array = {
+                { 10, 7 }
+            };
+            int expected = 10;
+
+            double actual = lab.SecondFunction(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SecondFunctionTest2()
+        {
+            Lab lab = new Lab();
+
+            double[,] array = {
+                { -2, 11, 3 },
+                { 17, 8, -9 },
+                { 6, 7, -1 }
+            };
+            int expected = 14;
+
+            double actual = lab.SecondFunction(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThirdFunctionTest1()
+        {
+            Lab lab = new Lab();
+
+            double[,] array = { { 7 } };
+            int expected = 7;
+
+            double actual = lab.ThirdFunction(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThirdFunctionTest2()
+        {
+            Lab lab = new Lab();
+
+            double[,] array = {
+                {-4,-3, -2 },
+                {-1, 0, 1 },
+                {2, 3, 4 }
+            };
+            int expected = 4;
+
+            double actual = lab.ThirdFunction(array);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ 4th-year/stp/Lab2/Lab2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lab2
+{
+    public class Lab
+    {
+        /*
+         * Поиск минимума из трёх чисел.
+         */
+        public int FirstFunction(int num1, int num2, int num3)
+        {
+            int min = num1;
+
+            if (num2 < min)
+                min = num2;
+
+            if (num3 < min)
+                min = num3;
+
+            return min;
+        }
+
+        /*
+         * Функция получает двумерный массив вещественных переменных A.
+         * Отыскивает и возвращает сумму значений компонентов массива, у
+         * которых сумма значений индексов – чётная.
+         */
+        public double SecondFunction(double[,] array)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    if ((i + j) % 2 == 0)
+                        sum += array[i, j];
+
+            return sum;
+        }
+
+        /*
+         * Функция получает двумерный массив вещественных переменных A.
+         * Отыскивает и возвращает максимальное значение компонентов массива,
+         * лежащих на и ниже главной диагонали.
+         */
+        public double ThirdFunction(double[,] array)
+        {
+            double max = array[0, 0];
+
+            for (int i = 1; i < array.GetLength(0); i++)
+                for (int j = 0; j <= i && j < array.GetLength(1); j++)
+                    if (max < array[i, j])
+                        max = array[i, j];
+
+            return max;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
